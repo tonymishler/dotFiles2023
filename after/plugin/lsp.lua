ab3866ab2372a03d297ecb79dce4d109963a3274
Tony Mishler <tony.mishler@gmail.com>
2023-01-25 08:11:10 -0600

    tweaks to shortcuts, removing some lsps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,8 +5,6 @@ lsp.preset("recommended")
 lsp.ensure_installed({
   'tsserver',
   'eslint',
-  'sumneko_lua',
-  'rust_analyzer',
 })
 
 -- Fix Undefined global 'vim'
--- after/plugin/omnisharp.lua
+++ after/plugin/omnisharp.lua
@@ -1,3 +1,6 @@
 vim.keymap.set("n","<leader>gi", "<cmd>:OmniSharpFindImplementations<CR>")
 vim.keymap.set("n", "<leader><space>", ":OmniSharpGetCodeActions<CR>")
+vim.keymap.set("n", "<leader>uu", ":OmniSharpFindUsages<CR>")
+
+
 
