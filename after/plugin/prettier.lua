20b0cf3d7519ca9eb554f59a9c0631a6252a258b
Tony Mishler <tony.mishler@gmail.com>
2023-02-17 15:31:08 -0600

    adding lsp sage, removing js from prettier config

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -1,2 +1,3 @@
 --configure prettier to run on save for js, jsx, ts, tsx, json, css, scss, html, csharp and graphql files
-vim.cmd [[autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx,*.json,*.css,*.scss,*.html,*.cs,csharpharp*.graphql Prettier]]
+--vim.cmd [[autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx,*.json,*.css,*.scss,*.html,*.cs,csharpharp*.graphql Prettier]]
+vim.cmd [[autocmd BufWritePre *.ts,*.tsx,*.json,*.css,*.scss,*.html,*.cs,csharpharp*.graphql Prettier]]
--- lua/mod2/packer.lua
+++ lua/mod2/packer.lua
@@ -65,4 +65,19 @@ return require('packer').startup(function(use)
     --insta vim prettier
     use 'prettier/vim-prettier'
 
+
+    use({
+      "glepnir/lspsaga.nvim",
+      branch = "main",
+      config = function()
+        require("lspsaga").setup({})
+      end,
+      requires = {
+        {"nvim-tree/nvim-web-devicons"},
+        --Please make sure you install markdown and markdown_inline parser
+        {"nvim-treesitter/nvim-treesitter"}
+      }
+    })
+
+
 end)
